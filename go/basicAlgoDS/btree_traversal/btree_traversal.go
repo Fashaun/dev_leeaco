fd46d04084d5de79b81a442723e19eb884b0e3cc
ShaunCH_Lin <shaunch_lin@moxa.com>
2026-02-23 17:11:35 +0800

    [New Feature] btree_traversal
     Changes to be committed:
    	new file:   btree_traversal/btree_traversal.go

---
+++ go/basicAlgoDS/btree_traversal/btree_traversal.go
@@ -0,0 +1,47 @@
+package btree_traversal
+
+import "container/list"
+
+/* 二元樹節點結構體 */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/* 建構子 */
+func NewTreeNode(v int) *TreeNode {
+	return &TreeNode{
+		Left:  nil, // 左子節點指標
+		Right: nil, // 右子節點指標
+		Val:   v,   // 節點值
+	}
+}
+
+/* 層序走訪 */
+func LevelOrder(root *TreeNode) []any {
+	// 初始化佇列，加入根節點
+	queue := list.New()
+	queue.PushBack(root)
+	// 初始化一個切片，用於儲存走訪序列
+	nums := make([]any, 0)
+	for queue.Len() > 0 {
+		// 隊列出隊
+		node := queue.Remove(queue.Front()).(*TreeNode)
+		// 儲存節點值
+		nums = append(nums, node.Val)
+		if node.Left != nil {
+			// 左子節點入列
+			queue.PushBack(node.Left)
+		}
+		if node.Right != nil {
+			// 右子節點入列
+			queue.PushBack(node.Right)
+		}
+	}
+	return nums
+}
+
+func Run() {
+
+}
